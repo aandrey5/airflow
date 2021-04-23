bbfcbb55b8c05bd660c96e1557c7abfd327492c0
aandrey5 <aandrey5@gmail.com>
2021-04-23 13:57:43 +0300

    fixed problem to_sql, added parallelism

--- dags/002_TaskflowApi.py
+++ dags/002_TaskflowApi.py
@@ -16,65 +16,61 @@ def TaskFlowApi():
    
     @task()
     def download_titanic_dataset():
-        url = 'https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
-        df = pd.read_csv(url)
-        df.to_csv(get_path('titanic.csv'), encoding='utf-8')
+        if get_path('titanic.csv'):
+            url = 'https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv'
+            df = pd.read_csv(url)
+            df.to_csv(get_path('titanic.csv'), encoding='utf-8')
+        else: df = pd.read_csv(get_path('titanic.csv'), encoding='utf-8')
+
         df_xcom = df.to_json(orient='table')
-        #context['task_instance'].xcom_push('titanic', value=df_xcom)
         dataset_df = df_xcom
         return dataset_df
 
     @task()
-    def mean_fare_per_class():
-        titanic_df = pd.read_csv(get_path('titanic.csv'))
+    def mean_fare_per_class(data):
+        titanic_df = pd.read_json(data, orient='table')
         avg_df = titanic_df.groupby(['Pclass']).mean('Fare')
         avg_result = avg_df[['Fare']]
-        #avg_result.to_csv(get_path('titanic_mean_fares.csv'))
         avg_xcom = avg_result.to_json(orient='table')
-        #context['task_instance'].xcom_push('mean_class', value=avg_xcom)
         dataset_mean = avg_xcom
+        
         return dataset_mean
 
     @task()
-    def pivot_dataset():
-        titanic_df = pd.read_csv(get_path('titanic.csv'))
+    def pivot_dataset(data):
+        titanic_df = pd.read_json(data, orient='table')
+        titanic_df['Pclass'] = titanic_df['Pclass'].astype(str)
         df = titanic_df.pivot_table(index=['Sex'],
                                     columns=['Pclass'],
                                     values='Name',
                                     aggfunc='count').reset_index()
-        #df.to_csv(get_path('titanic_pivot.csv'))
+      
+        df = df.fillna(0)
+       # titanic_df.to_csv(get_path('pivot_control.csv'), encoding='utf-8')
         dataset_pivot = df.to_json(orient='table')
         return dataset_pivot
 
 
     @task()
-    def push_to_postgresql(data):
+    def push_to_postgresql(name, data):
        # create sql engine for sqlalchemy
         db_string_airflow = 'postgresql://airflow_xcom:1q2w3e4r5T@10.128.100.98/dwh'
         engine = create_engine(db_string_airflow)
        # read dataframes from previous returned json data  
         avg_pull_xcom_df = pd.read_json(data, orient='table')
-       # df_pull_xcom_df = pd.read_json(dataset_df, orient='table')
-       # pivot_pull_xcom_df = pd.read_json(dataset_pivot, orient='table')
-       # get names of tables from variables
-       # avg_table = Variable.get(f'{data}')
-       # df_table = Variable.get('table_dwh_data')
-       # pivot_table = Variable.get('table_dwh_pivot')
-    
        # push data to postgresql
-        avg_pull_xcom_df.to_sql(f'data', con=engine, if_exists='replace')
-       # df_pull_xcom_df.to_sql(df_table, con=engine, if_exists='replace')
-       # pivot_pull_xcom_df.to_sql(pivot_table, con=engine, if_exists='replace')
+        avg_pull_xcom_df.to_sql(name, con=engine, if_exists='replace')
+       
 
     
     dataset_df =  download_titanic_dataset()
-    push_df =  push_to_postgresql(dataset_df)
+    push_df =  push_to_postgresql('dataset_df',dataset_df)
 
-    dataset_mean  = mean_fare_per_class()
-    push_mean = push_to_postgresql(dataset_mean)
+    dataset_mean  = mean_fare_per_class(dataset_df)
+    push_mean = push_to_postgresql('dataset_mean', dataset_mean)
     
-    dataset_pivot = pivot_dataset()
-    push_pivot = push_to_postgresql(dataset_pivot)
+    dataset_pivot = pivot_dataset(dataset_df)
+    push_pivot = push_to_postgresql('dataset_pivot', dataset_pivot)
 
     
 Taskflow = TaskFlowApi()
